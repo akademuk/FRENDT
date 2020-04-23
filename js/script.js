d6edc355138df7daa4740135d50d1c333fd5c996
Oleh Fedorov <akademuk24@gmail.com>
2020-04-24 02:02:04 +0300

    testing2

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    vertical: true,
+    centerMode: true,
+    focusOnSelect: true,
+  });
+});
