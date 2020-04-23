5cb19a903577dcf65bc252dd3a1b42a6fab0ed57
Oleh Fedorov <akademuk24@gmail.com>
2020-04-23 16:59:27 +0300

    testing

--- script/vertical-slider.js
+++ script/vertical-slider.js
@@ -4,11 +4,7 @@ $(document).ready(function () {
     dots: true,
     adaptiveHeight: true,
     vertical: true,
-    verticalSwiping: true,
     slidesToScroll: 1,
-    centerMode: true,
     focusOnSelect: true,
-    speed: 1500,
-    cssEase: "linear",
   });
 });
